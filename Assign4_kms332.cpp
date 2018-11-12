a6fadea68941fecfb773156c4bb4d3c94ed3b485
Kyle Shorter <44951956+Kyleshorter@users.noreply.github.com>
2018-11-11 19:29:05 -0600

    Update Assign4_kms332.cpp

--- Assign4_kms332.cpp
+++ Assign4_kms332.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include "StringList.h"
+using namespace std;
 
+StringList::StringList()
+{
+    StringNode *head = NULL;
+}
+StringList::~StringList()
+{
+    StringNode *del = head;
+    head = head -> next;
+    delete del;
+}
+int StringList::count()
+{
+   int count =0;
+   StringNode *current;
+   while(current !=NULL)
+    {
+        count++;
+        current = current->next;
+    }
+    cout << count << endl;
+}
+
+void StringList::add(string movie)
+{
+    StringNode *newNode = new StringNode;
+    newNode ->data = movie;
+    newNode ->next= NULL;
+}
+
+void StringList::display()
+{
+    StringNode *display;
+    display=head;
+    while (display!=NULL)
+    {
+        cout << display ->data << " ";
+        display=display->next;
+    }
+}
